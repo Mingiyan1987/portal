24de367dd58ea585c615e9d7966e4dbebe0cabc0
Mingiyan1987 <basanov.mt87@gmail.com>
2019-03-02 10:06:43 +0300

    first commit

---
+++ src/main/java/ru/basanov/App.java
@@ -0,0 +1,12 @@
+package ru.basanov;
+
+/*
+ * @author Mingiyan Basanov
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/main/java/ru/basanov/configuration/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package ru.basanov.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.basanov")
+@Import(AppConfig.class)
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
